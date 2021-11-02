b0d1a70077a705fcf5b4d1a2d46931be67b6e25e
humrenan <humrenan@gmail.com>
2021-11-01 23:10:47 -0400

    chore: add router

---
+++ lib/banking_web/router.ex
@@ -0,0 +1,25 @@
+defmodule BankingWeb.Router do
+  use BankingWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", BankingWeb do
+    pipe_through :api
+
+    post "/users", UsersController, :create
+  end
+
+  # Enables the Swoosh mailbox preview in development.
+  #
+  # Note that preview only shows emails that were sent by the same
+  # node running the Phoenix server.
+  if Mix.env() == :dev do
+    scope "/dev" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
