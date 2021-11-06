a17fe7abe258516c29f45958c59c0081d49a3dfc
humrenan <humrenan@gmail.com>
2021-11-06 15:35:49 -0400

    chore: add users view

---
+++ lib/banking_web/views/users_view.ex
@@ -0,0 +1,33 @@
+defmodule BankingWeb.UsersView do
+  use BankingWeb, :view
+  alias Banking.Users.Inputs.{User}
+  alias Banking.Accounts.Inputs.{Account}
+
+  def render("create.json", %{
+        user: %User{
+          account: %Account{id: account_id, balance: balance},
+          id: id,
+          name: name
+        }
+      }) do
+    %{
+      message: "User created!",
+      user: %{
+        id: id,
+        name: name,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
+      }
+    }
+  end
+end
+
+# %{
+#   user: %User{
+#     account: %Account{id: account_id, balance: balance},
+#     id: id,
+#     name: name
+#   }
+# }
