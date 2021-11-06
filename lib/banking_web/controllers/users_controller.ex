99d01ba20371432fb9ed3fa21a4d5a8a02d49410
humrenan <humrenan@gmail.com>
2021-11-06 15:39:44 -0400

    chore: change status

--- lib/banking_web/controllers/users_controller.ex
+++ lib/banking_web/controllers/users_controller.ex
@@ -1,4 +1,4 @@
-defmodule Banking.UsersController do
+defmodule BankingWeb.UsersController do
   use BankingWeb, :controller
 
   alias Banking
@@ -9,7 +9,7 @@ defmodule Banking.UsersController do
   def create(conn, params) do
     with {:ok, %User{} = user} <- Banking.create_user(params) do
       conn
-      |> put_status(:created)
+      |> put_status(200)
       |> render("create.json", user: user)
     end
   end
